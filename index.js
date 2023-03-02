6c240bf0207b36ecf660cb4c107f892780ce49e3
Nico Nussmüller <nico@nussmueller.dev>
2023-03-02 23:16:06 +0100

    feat: upgraded code

--- index.js
+++ index.js
@@ -1,61 +1,128 @@
-const SpotifyWebApi = require('spotify-web-api-node');
+const Client = require('spotify-web-api-node');
+const express = require('express');
 
-const spotifyApi = new SpotifyWebApi({
-    clientId: '31447e0990434b3cb640203f4416b621',
-    clientSecret: '5da32882dc5f40c8afe8d628c83c0e92'
+const client_id = '<your_client_id>';
+const client_secret = '<your_client_secret>';
+const redirect_uri = 'http://localhost:8888/callback';
+const scopes = ['user-library-read', 'playlist-modify-private'];
+
+const spotifyApi = new Client({
+    clientId: client_id,
+    clientSecret: client_secret,
+    redirectUri: redirect_uri
 });
 
-async function setAccesToken() {
-    let credentialResponse = await spotifyApi.clientCredentialsGrant();
-
-    console.log('The access token is ' + credentialResponse.body['access_token']);
-    spotifyApi.setAccessToken(credentialResponse.body['access_token']);
-}
-
-async function createLikedTracksPlaylist() {
-    const playlistName = 'My Liked Tracks';
-
-    console.log('test');
-    const playlists = await spotifyApi.getUserPlaylists();
-    const playlist = playlists.body.items.find((item) => item.name === playlistName);
-
-    if (playlist) {
-        console.log(`Playlist "${playlistName}" already exists!`);
-        return playlist.id;
-    }
-
-    const createdPlaylist = await spotifyApi.createPlaylist(playlistName, { 'description': 'A playlist of all my liked tracks' });
-    console.log(`Playlist "${playlistName}" created!`);
-    return createdPlaylist.body.id;
-}
-
-async function addLikedTracksToPlaylist(playlistId) {
-    const likedTracks = await spotifyApi.getMySavedTracks();
-
-    const trackIds = likedTracks.body.items.map((item) => item.track.id);
-
-    const playlistTracks = await spotifyApi.getPlaylistTracks(playlistId);
-    const playlistTrackIds = playlistTracks.body.items.map((item) => item.track.id);
-
-    const newTrackIds = trackIds.filter((id) => !playlistTrackIds.includes(id));
-
-    if (newTrackIds.length === 0) {
-        console.log('No new liked tracks to add to the playlist.');
+const app = express();
+app.get('/callback', async (req, res) => {
+    const error = req.query.error;
+    const code = req.query.code;
+    if (error) {
+        console.error('Authorization failed:', error);
+        res.send('Authorization failed. Please try again.');
         return;
     }
+    if (!code) {
+        console.error('Authorization failed: no code provided');
+        res.send('Authorization failed. Please try again.');
+        return;
+    }
+    try {
+        const data = await spotifyApi.authorizationCodeGrant(code);
+        const access_token = data.body['access_token'];
+        const refresh_token = data.body['refresh_token'];
+        console.log('Authorization successful!');
+        console.log('Access token:', access_token);
+        console.log('Refresh token:', refresh_token);
 
-    const response = await spotifyApi.addTracksToPlaylist(playlistId, newTrackIds);
-    console.log(`${response.body.tracks.length} tracks added to the playlist!`);
+        spotifyApi.setAccessToken(access_token);
+        spotifyApi.setRefreshToken(refresh_token);
+
+        server.close();
+    } catch (err) {
+        console.error('Authorization failed:', err);
+        res.send('Authorization failed. Please try again.');
+    }
+});
+
+async function authorizeApp() {
+    const authorizeURL = spotifyApi.createAuthorizeURL(scopes);
+    const server = app.listen(8888, () => {
+        console.log('Please authorize this app on the following page:');
+        console.log(authorizeURL);
+    });
+    await new Promise(resolve => server.on('close', resolve));
 }
 
-(async () => {
-    await setAccesToken();
+async function createPlaylistFromLikedSongs(playlistName) {
+    try {
+        if (!spotifyApi.getAccessToken()) {
+            await authorizeApp();
+        }
 
-    console.log(spotifyApi.getAccessToken());
-
-    createLikedTracksPlaylist()
-        .then((playlistId) => addLikedTracksToPlaylist(playlistId))
-        .catch((error) => {
-            console.error('Error occurred: ' + error);
+        let playlist = null;
+        const playlists = await spotifyApi.getUserPlaylists();
+        playlists.body.items.forEach(p => {
+            if (p.name === playlistName) {
+                playlist = p;
+            }
         });
-})();
+
+        if (!playlist) {
+            const user = await spotifyApi.getMe();
+            playlist = await spotifyApi.createPlaylist(user.body.id, playlistName, {
+                public: false
+            });
+        }
+        console.log(`Using playlist '${playlist.name}' (${playlist.id})`);
+
+        const pageSize = 50;
+        let offset = 0;
+        let allSongs = [];
+        do {
+            const { body } = await spotifyApi.getMySavedTracks({ limit: pageSize, offset });
+            allSongs = allSongs.concat(body.items);
+            offset += pageSize;
+        } while (allSongs.length < body.total);
+
+        console.log(`Retrieved ${allSongs.length} liked songs`);
+
+        const allLikedSongIds = allLikedSongs.map(song => song.track.id);
+        const playlistSongIds = allPlaylistSongs.map(song => song.track.id);
+        const songsToRemove = playlistSongIds.filter(songId => !allLikedSongIds.includes(songId));
+
+        if (songsToRemove.length > 0) {
+            console.log(`Removing ${songsToRemove.length} songs from playlist`);
+            await spotifyApi.removeTracksFromPlaylist(playlistId, songsToRemove.map(songId => playlistSongIds.first(x => x.track.id === songId)));
+        } else {
+            console.log('No songs to remove');
+        }
+
+        const playlistTracks = await spotifyApi.getPlaylistTracks(playlist.id, { limit: 100 });
+        const existingTracks = new Set();
+        playlistTracks.body.items.forEach(item => {
+            const track = item.track;
+            if (track) {
+                existingTracks.add(track.id);
+            }
+        });
+
+        const tracksToAdd = allSongs.filter(song => !existingTracks.has(song.track.id));
+        if (tracksToAdd.length === 0) {
+            console.log('All liked songs are already in the playlist');
+            return;
+        }
+        console.log(`Adding ${tracksToAdd.length} new songs to playlist`);
+
+        const chunkSize = 100;
+        for (let i = 0; i < tracksToAdd.length; i += chunkSize) {
+            const tracks = tracksToAdd.slice(i, i + chunkSize).map(song => song.track.uri);
+            await spotifyApi.addTracksToPlaylist(playlist.id, tracks);
+        }
+
+        console.log('Playlist updated successfully!');
+    } catch (err) {
+        console.error('An error occurred:', err);
+    }
+}
+
+createPlaylistFromLikedSongs('My Liked Songs');
