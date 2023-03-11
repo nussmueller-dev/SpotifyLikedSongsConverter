4066de784c004666ccb2ce6e0ae429810ab01528
Nico Nussmüller <nico@nussmueller.dev>
2023-03-11 02:03:45 +0100

    feat: better console informations

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import moment from "moment";
 import schedule from "node-schedule";
 import config from "./config/config.json";
 import { SpotifyHandler } from "./spotifyHandler";
@@ -8,7 +9,14 @@ async function main() {
   await spotifyHandler.syncLikedSongsToPlaylist(config.playListName);
 
   schedule.scheduleJob("*/15 * * * *", async function () {
+    console.log(`ScheduleJob at ${moment().format("DD.MM.YYYY HH:mm:ss")}`);
+    console.log(
+      "------------------------------------------------------------------"
+    );
     await spotifyHandler.syncLikedSongsToPlaylist(config.playListName);
+    console.log(
+      "------------------------------------------------------------------"
+    );
   });
 }
 
