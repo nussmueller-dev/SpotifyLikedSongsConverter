a70055481540010ec33969e2a7feded51f861629
Nico Nussmüller <nico@nussmueller.dev>
2023-03-11 01:55:21 +0100

    feat: cron-job

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+import schedule from "node-schedule";
+import config from "./config/config.json";
 import { SpotifyHandler } from "./spotifyHandler";
 
 const spotifyHandler = new SpotifyHandler();
+
+async function main() {
+  await spotifyHandler.syncLikedSongsToPlaylist(config.playListName);
+
+  schedule.scheduleJob("*/15 * * * *", async function () {
+    await spotifyHandler.syncLikedSongsToPlaylist(config.playListName);
+  });
+}
+
+main();
